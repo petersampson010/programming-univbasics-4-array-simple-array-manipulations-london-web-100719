7261ed01231ff8b2330628e65bd01f7539302522
Peter Sampson <petersampson010@gmail.com>
2019-08-29 12:18:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,20 @@ def pop_with_args(array)
   array.pop(-2)
 end 
 
-def
+def using_shift(array)
+  return array.shift()
+end 
+
+def shift_with_args(array)
+  return array.shift(2)
+end 
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end 
+
+
+
+
+
+
